d2e4991f14d7162b82f1e9b663b9b59ba2f131ec
davidleg <43000689+davidleg@users.noreply.github.com>
2018-09-05 15:46:08 +0200

    changes the delete process, now, ask to confirm deletion

--- php_avec_module_connexion/App/Frontend/Modules/News/Views/index.php
+++ php_avec_module_connexion/App/Frontend/Modules/News/Views/index.php
@@ -1,8 +1,13 @@
+<p style="text-align: center">Il y a actuellement <?= $nombreNews ?> news. En voici la liste :</p>
+
+<table>
+  <tr><th>Auteur</th><th>Titre</th><th>Date d'ajout</th><th>Dernière modification</th><th>Action</th></tr>
 <?php
 foreach ($listeNews as $news)
 {
-?>
-  <h2><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
-  <p><?= nl2br($news['contenu']) ?></p>
-<?php
+  echo '<tr><td>', $news['auteur'], '</td><td>', $news['titre'], '</td><td>le ', $news['dateAjout']->format('d/m/Y à H\hi'), '</td><td>', ($news['dateAjout'] == $news['dateModif'] ? '-' : 'le '.$news['dateModif']->format('d/m/Y à H\hi')), '</td>
+  <td><a href="news-update-', $news['id'], '.html"><img src="/images/update.png" alt="Modifier" /></a> 
+      <a href="news-delete-ask-', $news['id'], '.html"><img src="/images/delete.png" alt="Supprimer" /></a></td></tr>', "\n";
 }
+?>
+</table>
